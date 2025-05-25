f3028739285a09573ce668caabe584011151a129
MANOPRIYAN <86639504+manopriyan6@users.noreply.github.com>
2025-05-26 01:34:26 +0530

    Remove scroll down indicator from Hero

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -44,14 +44,6 @@ const Hero: React.FC = () => {
           </div>
         </div>
       </div>
-      
-      {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
-        <span className="text-gray-400 text-sm mb-2">Scroll Down</span>
-        <svg className="w-6 h-6 text-gray-400" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
-          <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-        </svg>
-      </div>
     </div>
   );
 };
